ede499df956d9386a436b36eccd351e5f6b06cb5
tobiasleitner5 <76008705+tobiasleitner5@users.noreply.github.com>
2020-12-15 18:45:01 +0100

    delete

--- ScrumTaskboard-KFG/01 Compile ScrumTaskboard.sh
+++
@@ -1,6 +0,0 @@
-export JAVA_HOME=/Library/Java/Home
-echo $JAVA_HOME
-
-java -jar Coco.jar ScrumTaskboard.atg -frames frm -o src
-javac -d cls src/Parser.java src/Scanner.java src/ScrumTaskboard.java 
-jar cfmv ScrumTaskboard.jar ScrumTaskboard.mf -C cls .
--- ScrumTaskboard-KFG/02 Execute ScrumTaskboard.sh
+++
@@ -1,4 +0,0 @@
-export JAVA_HOME=/Library/Java/Home
-echo $JAVA_HOME
-
-java -jar ScrumTaskBoard.jar ScrumTaskBoard-input.txt
